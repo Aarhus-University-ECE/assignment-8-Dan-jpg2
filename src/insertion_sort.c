7b605f689637860661aec97ff0232d33f6c5c41c
DanielP <Danielpihl22@gmail.com>
2022-10-27 23:20:24 +0200

    WIP

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -5,18 +5,18 @@
 
 void sort(linked_list *llPtr)
 {
-    node_t *pointer = llPtr->head;
-    node_t *temporary;
+    node_t *pointer = llPtr->head; //keeping track of current node
+    node_t *temporary; 
     node_t *temporary_pointer = llPtr->head;
 
-    while(pointer -> next != NULL) //Loop runs as long as there is a value to allocate
+    while(pointer -> next != NULL) //Loop runs as long as we are not at the end of list
     {
-        if(pointer -> data >= (pointer -> next -> data)) //if current data is bigger find place
+        if(pointer -> data >= (pointer -> next -> data)) //if current data is bigger than next move along
         {
             temporary = pointer -> next;
             printLL(llPtr); 
             pointer -> next = pointer -> next -> next;
-            while(temporary_pointer != pointer -> next)
+            while(temporary_pointer != pointer -> next) //
             {
                 if(temporary -> data <= temporary_pointer -> data)
                 {
@@ -34,49 +34,7 @@ void sort(linked_list *llPtr)
             }
             temporary_pointer = llPtr -> head;
         }
-        else
+        else 
             pointer = pointer -> next; 
     }
 }
-    /*/ Add your sort function here
-node_t *temp = NULL;
-node_t *temp_next = NULL;
-node_t *previous = llPtr->head;
-node_t *current = previous->next;
-
-if(llPtr->head == NULL)
-    {
-        printf("list is empty");
-        return; 
-    }
-
-
-while (previous->next != NULL) //Loop runs as long as there is a next node to allocate
-{
-    if(current->data < previous->data) // If current node is less or equal than previous, find place. 
-    {
-        temp = current; 
-        temp_next = current->next;
-        previous->next = current->next; 
-
-        node_t *placement = llPtr->head;
-        while(placement->next->data < temp->data)
-        {
-            placement = placement->next;
-        }
-        if(llPtr->head == placement)
-        {
-            temp->next = llPtr->head;
-            llPtr->head = temp;
-
-        }
-            else 
-            {
-                temp_next->next = placement->next;
-                placement->next = temp;
-            }
-    }
-    
-    previous = current;
-    current = previous->next;
-    } */
--- src/list_queue.c
+++ src/list_queue.c
@@ -6,39 +6,39 @@ void init_queue(queue *q)
 {
   // Add your init_queue
   //Initialize of the queue
-  q->front = NULL;
-  q->rear = NULL; 
-  q->size = 0;
+  q->front = NULL; //
+  q->rear = NULL; //rear pointer has no value
+  q->size = 0; //set size to 0
 }
 
 int empty(queue *q)
 {
   // Add your empty function
-  return(q -> size <= 0);  
+  return(q -> size <= 0); //queue size has to be zero or less to be empty.
 }
 
 void enqueue(queue *q, int x)
 {
   // Add your enqueue function
-  qnode *new = malloc(sizeof(qnode));
+  qnode *new = malloc(sizeof(qnode)); //Creating new node 
   if(q -> rear == NULL)
   {
-    q -> rear = new;
-    q -> rear -> data = x;
+    q -> rear = new; //new node is q's last node
+    q -> rear -> data = x; //add data to new node 
     q -> size++;
   }
   else if(q -> front == NULL)
   {
     q -> front = new;
-    q -> front -> data = x;
+    q -> front -> data = x; //add data to new node
     q -> rear -> next = q -> front; 
     q -> size++;
   }
   else
   {
-    q -> front -> next = new;
+    q -> front -> next = new; 
     q -> front = new;
-    q -> front -> data = x;
+    q -> front -> data = x; //add data to new node
     q -> size++;
   }
 
@@ -47,9 +47,9 @@ int dequeue(queue *q)
 {
   // Add your dequeue function
   int return_value = q -> rear -> data;
-  qnode *temp = q -> rear;
-  q -> rear = q -> rear -> next;
-  q -> size = q -> size - 1;
+  qnode *temp = q -> rear; //starting at rear of queue
+  q -> rear = q -> rear -> next; 
+  q -> size = q -> size - 1; //after dequeue removing 1 from size
   free(temp);
   return return_value;
 }
