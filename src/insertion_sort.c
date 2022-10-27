393468cb7e2d69f85873200b913567c430bcf8ea
DanielP <Danielpihl22@gmail.com>
2022-10-27 18:08:32 +0200

    WIP

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -5,5 +5,78 @@
 
 void sort(linked_list *llPtr)
 {
- // Add your sort function here
+    node_t *pointer = llPtr->head;
+    node_t *temporary;
+    node_t *temporary_pointer = llPtr->head;
+
+    while(pointer -> next != NULL) //Loop runs as long as there is a value to allocate
+    {
+        if(pointer -> data >= (pointer -> next -> data)) //if current data is bigger find place
+        {
+            temporary = pointer -> next;
+            printLL(llPtr); 
+            pointer -> next = pointer -> next -> next;
+            while(temporary_pointer != pointer -> next)
+            {
+                if(temporary -> data <= temporary_pointer -> data)
+                {
+                    temporary -> next = temporary_pointer;
+                    llPtr -> head = temporary;
+                    break;
+                }
+                else if ((temporary -> data) <= (temporary_pointer -> next -> data))
+                {
+                    temporary -> next = temporary_pointer -> next;
+                    temporary_pointer -> next = temporary;
+                    break;
+                }
+                temporary_pointer = temporary_pointer -> next;
+            }
+            temporary_pointer = llPtr -> head;
+        }
+        else
+            pointer = pointer -> next; 
+    }
 }
+    /*/ Add your sort function here
+node_t *temp = NULL;
+node_t *temp_next = NULL;
+node_t *previous = llPtr->head;
+node_t *current = previous->next;
+
+if(llPtr->head == NULL)
+    {
+        printf("list is empty");
+        return; 
+    }
+
+
+while (previous->next != NULL) //Loop runs as long as there is a next node to allocate
+{
+    if(current->data < previous->data) // If current node is less or equal than previous, find place. 
+    {
+        temp = current; 
+        temp_next = current->next;
+        previous->next = current->next; 
+
+        node_t *placement = llPtr->head;
+        while(placement->next->data < temp->data)
+        {
+            placement = placement->next;
+        }
+        if(llPtr->head == placement)
+        {
+            temp->next = llPtr->head;
+            llPtr->head = temp;
+
+        }
+            else 
+            {
+                temp_next->next = placement->next;
+                placement->next = temp;
+            }
+    }
+    
+    previous = current;
+    current = previous->next;
+    } */
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
 extern "C"{
     // Add the header files required to run your main 
+    #include "insertion_sort.h"
+    #include "linked_list.h"
+    #include "list_queue.h"
+
 }
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
+
+    int testlist[] ={4,3,10,1,6};
+    {
+    linked_list* test = createLinkedList();
+    for (int i = 0; i < 5; ++i)
+    {
+        insertFront(createNode(testlist[5-i-1]), test);
+    }
+    
+    printLL(test);
+    sort(test);
+    printLL(test);
+    queue q;
+    init_queue(&q);
+    printf("%d\n", empty(&q));
+
+    //Above test is my test for sorting a list
+
+   /* queue q;
+    init_queue(&q);
+    printf("%d\n", empty(&q));
+    enqueue(&q, 10);
+    enqueue(&q, 5);
+    enqueue(&q, 15);
+    printf("%d\n", empty(&q));
+    printf("%d\n", q.front->data);
+    //printf("%d", q.front->next->data);
+    printf("%d", q.rear->data);
+    printf("%d\n", dequeue(&q));
+    printf("%d\n", dequeue(&q));
+    printf("%d", empty(&q));*/
     return 0;
+
+}
 }
